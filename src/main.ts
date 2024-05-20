9fe9e7d982ae7abad2b19cbe5a7d176ecf2c7771
Daiane Bolzan <dainha85@gmail.com>
2024-05-20 20:19:49 -0300

    feat: Add Swagger documentation to the API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerDocumentConfig } from './doc/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+
+  app.enableCors();
+
+  const document = SwaggerModule.createDocument(app, swaggerDocumentConfig);
+  SwaggerModule.setup('', app, document);
 }
 bootstrap();
